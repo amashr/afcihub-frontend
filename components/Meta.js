b6fe0872e30a32c16111714f048b8ecc96ebfa39
FrugenceFide <frugencefidel@gmail.com>
2019-09-30 15:58:42 +0300

    Add favicon

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => (
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-    {/* <link rel="shortcut icon" href="/static/favicon.png" /> */}
+    <link rel="shortcut icon" href="/static/favicon.png" />
     <link rel="stylesheet" href="/static/nprogress.css" />
     <link
       href="https://fonts.googleapis.com/css?family=Karla:400,400i%7CRoboto:100,300,400,700,900"
