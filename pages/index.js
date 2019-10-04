32c01af1676559e81f8722d163acdfd37a043faa
Ally Maumba <amaumba1@gmail.com>
2019-10-04 06:24:58 -0500

    auth token

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Main from '../components/Main';
+import {  getTokenForBrowser } from '../static/auth'
+import template  from '../static/template'
+import PropTypes from 'prop-types'
 
-const Index = () => (
-  <>
-    <Main />
-  </>
-);
+const Index = ({isLoggedIn}) => (
+  <div>
+    { !isLoggedIn && (
+      <Main />
+    )};
+  </div>
+)
 
-export default Index;
+Index.propTypes = {
+  isLoggedIn: PropTypes.bool
+}
+
+export default template(Index);
